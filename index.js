8fb5f94c04d4a53b06b28b39c8af876ebc943172
Juan <juandavidmengar@gmail.com>
2023-11-22 13:33:01 -0500

    cors permite el propio origen

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler.js');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middleware nativos de expres
 app.use(express.json()); //permite recibir informacion de tipo json
@@ -13,7 +13,7 @@ const whiteList =  ['http://localhost:8080', 'https://myapp.co'];
 
 const options = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error("No permitido"));
